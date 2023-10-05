732adc8b8efd0a596cfc9bdaa9d6d82279d39212
EvgeniyaZz <zakharova.evgeniya.19@gmail.com>
2023-10-05 10:41:12 +0300

    homework (lesson 10) - рефакторинг doRead

--- src/com/urise/webapp/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/serializer/DataStreamSerializer.java
@@ -19,36 +19,32 @@ public class DataStreamSerializer implements Serializer {
             dos.writeUTF(r.getFullName());
 
             Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
             writeWithException(contacts.entrySet(), dos, entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             });
 
             Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
             writeWithException(sections.entrySet(), dos, entry -> {
                 SectionType sectionType = entry.getKey();
+                Section section = entry.getValue();
                 dos.writeUTF(sectionType.name());
                 switch (sectionType) {
                     case PERSONAL:
                     case OBJECTIVE:
-                        dos.writeUTF(entry.getValue().toString());
+                        dos.writeUTF(section.toString());
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS: {
-                        ListSection listSection = (ListSection) entry.getValue();
+                        ListSection listSection = (ListSection) section;
                         List<String> list = listSection.getList();
-                        dos.writeInt(list.size());
                         writeWithException(list, dos, dos::writeUTF);
                         break;
                     }
                     case EXPERIENCE:
                     case EDUCATION: {
-                        CompanySection companySection = (CompanySection) entry.getValue();
+                        CompanySection companySection = (CompanySection) section;
                         List<Company> companies = companySection.getCompanies();
-                        int size = companies.size();
-                        dos.writeInt(size);
                         writeWithException(companies, dos, company -> {
                             Link homePage = company.getHomePage();
                             dos.writeUTF(homePage.getName());
@@ -60,8 +56,6 @@ public class DataStreamSerializer implements Serializer {
                                 dos.writeInt(0);
                             }
                             List<Company.Period> periods = company.getPeriods();
-                            int sizePeriods = periods.size();
-                            dos.writeInt(sizePeriods);
                             writeWithException(periods, dos, period -> {
                                 dos.writeInt(period.getStartDate().getYear());
                                 dos.writeUTF(period.getStartDate().getMonth().name());
@@ -91,42 +85,33 @@ public class DataStreamSerializer implements Serializer {
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
 
-            int sizeContact = dis.readInt();
-            for (int i = 0; i < sizeContact; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
+            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
 
-            int sizeSection = dis.readInt();
-            for (int i = 0; i < sizeSection; i++) {
-                String sectionType = dis.readUTF();
+            readWithException(dis, () -> {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
-                    case "PERSONAL":
-                    case "OBJECTIVE":
-                        resume.addSection(SectionType.valueOf(sectionType), new TextSection(dis.readUTF()));
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        resume.addSection(sectionType, new TextSection(dis.readUTF()));
                         break;
-                    case "ACHIEVEMENT":
-                    case "QUALIFICATIONS": {
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS: {
                         List<String> list = new ArrayList<>();
-                        int sizeList = dis.readInt();
-                        for (int j = 0; j < sizeList; j++) {
-                            list.add(dis.readUTF());
-                        }
-                        resume.addSection(SectionType.valueOf(sectionType), new ListSection(list));
+                        readWithException(dis, () -> list.add(dis.readUTF()));
+                        resume.addSection(sectionType, new ListSection(list));
                         break;
                     }
-                    case "EXPERIENCE":
-                    case "EDUCATION": {
-                        int sizeList = dis.readInt();
+                    case EXPERIENCE:
+                    case EDUCATION: {
                         List<Company> companies = new ArrayList<>();
-                        for (int j = 0; j < sizeList; j++) {
+                        readWithException(dis, () -> {
                             String name = dis.readUTF();
                             String website = null;
                             if (dis.readInt() == 1) {
                                 website = dis.readUTF();
                             }
                             List<Company.Period> periods = new ArrayList<>();
-                            int sizePeriod = dis.readInt();
-                            for (int k = 0; k < sizePeriod; k++) {
+                            readWithException(dis, () -> {
                                 LocalDate startDate = LocalDate.of(dis.readInt(), Month.valueOf(dis.readUTF()), 1);
                                 LocalDate endDate = LocalDate.of(dis.readInt(), Month.valueOf(dis.readUTF()), 1);
                                 String title = dis.readUTF();
@@ -135,26 +120,39 @@ public class DataStreamSerializer implements Serializer {
                                     description = dis.readUTF();
                                 }
                                 periods.add(new Company.Period(startDate, endDate, title, description));
-                            }
+                            });
                             companies.add(new Company(new Link(name, website), periods));
-                        }
-                        resume.addSection(SectionType.valueOf(sectionType), new CompanySection(companies));
+                        });
+                        resume.addSection(sectionType, new CompanySection(companies));
                         break;
                     }
                 }
-            }
+            });
             return resume;
         }
     }
 
     @FunctionalInterface
-    public interface WriteElement<T> {
+    private interface WriteElement<T> {
         void write(T t) throws IOException;
     }
 
-    public <T> void writeWithException(Collection<T> collection, DataOutputStream dos, WriteElement<T> writeElement) throws IOException {
+    @FunctionalInterface
+    private interface ReadElement {
+        void read() throws IOException;
+    }
+
+    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, WriteElement<T> writeElement) throws IOException {
+        dos.writeInt(collection.size());
         for (T t : collection) {
             writeElement.write(t);
+        }
+    }
+
+    private void readWithException(DataInputStream dis, ReadElement readElement) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            readElement.read();
         }
     }
 }
