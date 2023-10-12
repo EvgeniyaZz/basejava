9d1abb8ce5092ce625c3f5c05d70b1c82b1c740a
EvgeniyaZz <zakharova.evgeniya.19@gmail.com>
2023-10-12 10:08:54 +0300

    homework (lesson 11) change

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -58,29 +58,8 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
-
-        // Homework
-        int a = 1;
-        int b = 2;
-        lockObject(a, b);
-        lockObject(b, a);
     }
 
-    private static void lockObject(Object a, Object b) {
-        new Thread(() -> {
-            synchronized (a) {
-                System.out.println(Thread.currentThread().getName() + " lock object " + a);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (b) {
-                    System.out.println("Deadlock didn't work out");
-                }
-            }
-        }).start();
-    }
 
     private synchronized void inc() {
 //        synchronized (this) {
